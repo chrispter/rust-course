15e5a66a9ea92f0a6fabd94b735b2d334c1831c6
Christian MSO <aranrisck@gmail.com>
2022-10-28 10:05:40 -0500

    0.1.1

---
+++ src/variables.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // variable inmutable
+    // let x = 3;
+    // println!("El valor de x es: {}", x);
+
+    // Variable mutable
+    // let mut x = 3;
+    // println!("El valor de x es: {}", x);
+    // x = 7;
+    // print!("El valor de x es: {}", x);
+
+    // variable constante
+    // const PI: f32 = 3.1416;
+    // print!("El valor de PI es: {}", PI);
+
+    // Cambiar el tipo de variable
+    let x = 3;
+    println!("El valor de x es: {}", x);
+    let x = "a";
+    println!("El valor de x es: {}", x);
+}
+
+
+// Declaración de variables
+
