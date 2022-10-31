952fb94b2ce6a4cbe8c21238a6ba015a80e5477c
Christian MSO <aranrisck@gmail.com>
2022-10-31 11:32:59 -0600

    0.1.2

---
+++ src/functions.rs
@@ -0,0 +1,37 @@
+fn main(){
+    //    println!("Hola mundo"); 
+        //    mi_funcion();
+        // value_variable(7, 9);
+    
+        // let i: i32 = 26;
+    
+        // let j = {
+        //     let k = i + 1;
+        //     k + 1
+        // };
+    
+        // println!("El valor de j es: {}", j);
+    
+    
+        let i = trece();
+        println!("El valor de i es: {}", i); 
+}
+    
+    // PRimera funcion
+    // fn mi_funcion (){
+    //     println!("Hola nueva funcion");
+    // }
+    
+    // parametros
+    // fn value_variable(i: i32, j:i32){
+    //     println!("El valor del parametro primer es: {}", i);
+    //     println!("El valor del parametro segundo es: {}", j)
+    // }
+    
+    
+    // funciones que devuelven valores
+    
+fn trece() -> i8 {
+    return 13;
+}
+    
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,5 @@
 fn main(){
-    // let mut x:i32 = 5;
-    // println!("The value of x is: {}", x);
-    // x = -366999;
-    // println!("The value of x is: {}", x);
-    // let decimal1 = 4.0;
-    // let decimal2 = -455.399;
-
-    // Operaciones basicas
-    let suma = 58 + 77;
-    let resta = 3245.55 - 788.23;
-    let multiplicacion = 455.3 * 23.0;
-    let division = 34 / 4;
-    let resto = 34 % 4;
-
-    // Tipo boolean
-    let verdadero: bool = true;
-    let falso: bool = false;
-
-    // Tipo caracter
-    let a: char = 'a';
-    let emoji: char = '😁';
-
-    // Tipos compuestos
-    // Tuplas y arrays
-
-    let tupla = (500.34, 57, -344.9 );
-
-    // desestructuración
-    let (x, y, z) = tupla;
-    println!("El segundo valor de la tupla es: {}", y);
-
-    // tipo array(matriz)
-    let matriz:[i32;4] = [23, 45, 2, -11];
-    let x: i32 = matriz[0];
-
-
+    println!("Hola mundo"); 
+  
 }
+
